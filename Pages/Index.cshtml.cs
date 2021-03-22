fe92a23957d5ed15751f27ffb2d8977a73d86e5f
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-03-22 19:43:00 +0100

    Dodaj pliki projektów.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using FizzBuzz.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace FizzBuzz.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public Number_Result Number_Result { get; set; }
+        public static List<Number_Result> PrevNum = new List<Number_Result>();
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet() { }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Number_Result.Check(Number_Result.Number);
+                Number_Result.Time = System.DateTime.Now;
+
+                PrevNum.Add(Number_Result);
+
+                HttpContext.Session.SetString("SessionNumber", JsonConvert.SerializeObject(PrevNum));
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Number.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using FizzBuzz.Models;
+using System.Collections.Generic;
+
+namespace FizzBuzz.Pages
+{
+    public class NumberModel : PageModel
+    {
+        public Number_Result Number_Result;
+        public static List<Number_Result> PrevNum = new List<Number_Result>();
+        
+        public void OnGet()
+        {
+            var SessionNumber = HttpContext.Session.GetString("SessionNumber");
+            if (SessionNumber != null)
+            {
+                PrevNum = JsonConvert.DeserializeObject<List<Number_Result>>(SessionNumber);
+            }
+        }
+    }
+}
